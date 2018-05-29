2579f7e7d951aea79c8236fb9d30a6fcdf08064a
extendedclip <fairviewent@yahoo.com>
2018-05-29 14:25:09 -0400

    Pass the collection of guilds to the saveGuilds method

--- src/main/java/me/faceguy/guilds/storage/guild/GuildStorage.java
+++ src/main/java/me/faceguy/guilds/storage/guild/GuildStorage.java
@@ -1,14 +1,14 @@
 package me.faceguy.guilds.storage.guild;
 
+import java.util.Collection;
 import java.util.Set;
-import me.faceguy.guilds.GuildsPlugin;
 import me.faceguy.guilds.data.Guild;
 import me.faceguy.guilds.managers.GuildManager;
 
 public interface GuildStorage {
   void onLoad(GuildManager manager);
   void onUnload();
-  void saveGuilds();
+  int saveGuilds(Collection<Guild> guilds);
   Set<Guild> loadGuilds();
   boolean saveGuild(Guild guild);
   Guild loadGuild(String name);
